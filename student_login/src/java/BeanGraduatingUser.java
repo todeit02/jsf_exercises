954af8cae1c56469fc634ed5cfeaa279b648d486
Tobias Deißler <tobias_christian.deissler@student.reutlingen-university.de>
2018-06-02 23:37:44 +0200

    Initial commit. Form elements for the login exercise.

---
+++ student_login/src/java/BeanGraduatingUser.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Tobias
+ */
+
+import javax.faces.bean.ManagedBean; 
+     
+
+@ManagedBean
+public class BeanGraduatingUser
+{
+    private String username;
+    private String degreeProgram;
+    private int graduationYear;
+    
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getDegreeProgram() { return degreeProgram; }
+    public void setDegreeProgram(String degreeProgram) { this.degreeProgram = degreeProgram; }
+
+    public int getGraduationYear() { return graduationYear; }
+    public void setGraduationYear(int graduationYear) { this.graduationYear = graduationYear; }
+}
