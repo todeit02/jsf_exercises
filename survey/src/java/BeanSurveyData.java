28fd2dd2b3de1623f787af657fea6f101ef1535d
Tobias Deißler <tobias_christian.deissler@student.reutlingen-university.de>
2018-06-03 18:09:54 +0200

    Cosmetic changes. Created project for survey.

---
+++ survey/src/java/BeanSurveyData.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author Tobias
+ */
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class BeanSurveyData
+{
+    
+}
