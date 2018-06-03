15f1108362ae8b8cd58845fc5f09fc3d30274227
Tobias Deißler <tobias_christian.deissler@student.reutlingen-university.de>
2018-06-03 17:23:33 +0200

    Layout improvement. Internationalization. Additional birth date entry.

--- student_login/src/java/BeanGraduatingUser.java
+++ student_login/src/java/BeanGraduatingUser.java
@@ -9,6 +9,7 @@
  * @author Tobias
  */
 
+import java.util.Date;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
@@ -19,6 +20,7 @@ public class BeanGraduatingUser
     private String username;
     private String degreeProgram;
     private int graduationYear;
+    private Date birthDate;
     
     public String getUsername() { return username; }
     public void setUsername(String username) { this.username = username; }
@@ -28,4 +30,7 @@ public class BeanGraduatingUser
 
     public int getGraduationYear() { return graduationYear; }
     public void setGraduationYear(int graduationYear) { this.graduationYear = graduationYear; }
+
+    public Date getBirthDate() { return birthDate; }
+    public void setBirthDate(Date birthDate) { this.birthDate = birthDate; }
 }
